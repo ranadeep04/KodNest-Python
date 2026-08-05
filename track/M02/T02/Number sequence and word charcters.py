7c212ea37762e3c1f53c273db45e96e6b0968c74
Ranadeep <ranadeepreddyk123@gmail.com>
2026-08-05 17:10:51 +0530

    For loop

---
+++ track/M02/T02/Number sequence and word charcters.py
@@ -0,0 +1,16 @@
+n=int(input("Enter the value of n:"))
+
+word=input()
+
+print("Numbers:")
+for i in range (1,n+1):
+    print(i)
+
+
+print("Characters")
+for char in word:
+    print(char)
+
+
+
+    
